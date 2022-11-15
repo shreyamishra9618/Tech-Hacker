282079e1908b0de84ae89cb5eb49346d475f5f7c
shreyamishra9618 <shreyamishra9618@gmail.com>
2022-11-15 12:17:35 -0800

    updating the structure

--- models/User.js
+++ models/User.js
@@ -43,12 +43,12 @@ User.init (
         hooks: {
           // set up beforeCreate lifecycle "hook" functionality
           async beforeCreate(newUserData) {
-            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            newUserData.password = await bcrypt.hash(newUserData.password, 8);
             return newUserData;
           },
     
           async beforeUpdate(updatedUserData) {
-            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 8);
             return updatedUserData;
           }
         },
